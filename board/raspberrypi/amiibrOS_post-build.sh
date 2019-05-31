2784b45e5e0542a0db5cb177c08b3fb7a778fcb4
Joseph <jpyankel@gmail.com>
2019-05-31 16:35:32 -0400

    Add amiibrOS-raylib Build Support

--- board/raspberrypi/amiibrOS_post-build.sh
+++ board/raspberrypi/amiibrOS_post-build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Copy over the rpi boot config file.
 cat amiibrOS_config.txt > ${BINARIES_DIR}/rpi-firmware/config.txt
+
+# Build raylib and install to target:
+cd ../raylib/src && make && cp ../build/*.so* ${TARGET_DIR}/usr/lib/
