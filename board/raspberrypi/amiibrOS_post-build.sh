355c4a93809b04d7433a3c798b5162fdac401b41
jpyankel <jpyankel@gmail.com>
2019-06-19 22:51:33 -0400

    Add os_ctrl and main_interface Build to amiibrOS_post-build.sh

--- board/raspberrypi/amiibrOS_post-build.sh
+++ board/raspberrypi/amiibrOS_post-build.sh
@@ -4,4 +4,13 @@
 cat amiibrOS_config.txt > ${BINARIES_DIR}/rpi-firmware/config.txt
 
 # Build raylib and install to target:
-cd ../raylib/src && make && cp ../build/*.so* ${TARGET_DIR}/usr/lib/
+(cd ../amiibrOS-raylib/src && make && cp ../build/*.so* ${TARGET_DIR}/usr/lib/)
+
+# Build amiibrOS system software and install to target:
+(cd ../subproj/main_interface && make rpi \
+  && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/main_interface \
+  && cp build/* ${TARGET_DIR}/usr/bin/amiibrOS/main_interface)
+
+(cd ../subproj/os_ctrl && make rpi \
+  && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/os_ctrl \
+  && cp build/* ${TARGET_DIR}/usr/bin/amiibrOS/os_ctrl)
