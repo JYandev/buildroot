4824777cd7b387bf7a9c973de5528d0e20341cf7
jpyankel <jpyankel@gmail.com>
2019-07-13 23:31:54 -0400

    Update Post-Build to Account for Repurposed amiibrOS Programs
    Replaced main_interface and os_ctrl building with the merge of the
    two programs relabeled as "amiibrOS".

--- board/raspberrypi/amiibrOS_post-build.sh
+++ board/raspberrypi/amiibrOS_post-build.sh
@@ -8,10 +8,6 @@ cat amiibrOS_config.txt > ${BINARIES_DIR}/rpi-firmware/config.txt
   ${TARGET_DIR}/usr/lib/)
 
 # Build amiibrOS system software and install to target:
-(cd ../subproj/main_interface && make clean && make rpi \
-  && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/main_interface \
-  && cp -r build/* ${TARGET_DIR}/usr/bin/amiibrOS/main_interface)
-
-(cd ../subproj/os_ctrl && make clean && make rpi \
-  && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/os_ctrl \
-  && cp -r build/* ${TARGET_DIR}/usr/bin/amiibrOS/os_ctrl)
+(cd ../subproj/amiibrOS && make clean && make rpi \
+  && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/ \
+  && cp -r build/* ${TARGET_DIR}/usr/bin/amiibrOS)
