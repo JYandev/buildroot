31dc965a4cd829d045d1c082a44a7b33920f8028
Joseph <jpyankel@gmail.com>
2019-07-14 22:50:55 -0400

    Correct Bug in Post-Build Script

--- board/raspberrypi/amiibrOS_post-build.sh
+++ board/raspberrypi/amiibrOS_post-build.sh
@@ -12,5 +12,5 @@ cat amiibrOS_config.txt > ${BINARIES_DIR}/rpi-firmware/config.txt
   && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/ \
   && cp -r build/* ${TARGET_DIR}/usr/bin/amiibrOS)
 (mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/amiibo_scan \
-  && cp ../subproj/amiibo_scan/amiibo_scan.py ${TARGET_DIR}/usr/bin/amiibrOS\
-  /amiibo_scan)
+  && cp ../subproj/amiibo_scan/amiibo_scan.py \
+  ${TARGET_DIR}/usr/bin/amiibrOS/amiibo_scan)
