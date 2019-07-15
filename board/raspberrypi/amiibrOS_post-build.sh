d5cfe5e846405025c7aa5fc5ab2e79fc0ca7ab39
jpyankel <jpyankel@gmail.com>
2019-07-14 20:28:57 -0400

    Add amiibo_scan.py Inclusion to Post-Build Script

--- board/raspberrypi/amiibrOS_post-build.sh
+++ board/raspberrypi/amiibrOS_post-build.sh
@@ -11,3 +11,6 @@ cat amiibrOS_config.txt > ${BINARIES_DIR}/rpi-firmware/config.txt
 (cd ../subproj/amiibrOS && make clean && make rpi \
   && mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/ \
   && cp -r build/* ${TARGET_DIR}/usr/bin/amiibrOS)
+(mkdir -p ${TARGET_DIR}/usr/bin/amiibrOS/amiibo_scan \
+  && cp ../subproj/amiibo_scan/amiibo_scan.py ${TARGET_DIR}/usr/bin/amiibrOS\
+  /amiibo_scan)
