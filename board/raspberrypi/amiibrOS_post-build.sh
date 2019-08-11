2a4c2028de139fa3195b1106746e0d8d2ac05549
Joseph <jpyankel@gmail.com>
2019-08-11 09:57:34 -0400

    Add Bluetooth Support and Sound Over HDMI

--- board/raspberrypi/amiibrOS_post-build.sh
+++ board/raspberrypi/amiibrOS_post-build.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-# Copy over the rpi boot config file.
+# ============
+set -u
+set -e
+
+# Add a console on tty1
+#if [ -e ${TARGET_DIR}/etc/inittab ]; then
+#  grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+#	sed -i '/GENERIC_SERIAL/atty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI \
+#    console' ${TARGET_DIR}/etc/inittab
+#fi
+# ============
+
+# Copy over the rpi boot config files.
 cat amiibrOS_config.txt > ${BINARIES_DIR}/rpi-firmware/config.txt
+cat amiibrOS_cmdline.txt > ${BINARIES_DIR}/rpi-firmware/cmdline.txt
 
 # Build raylib and install to target:
 (cd ../amiibrOS-raylib/src && make && cp -r ../build/*.so* \
